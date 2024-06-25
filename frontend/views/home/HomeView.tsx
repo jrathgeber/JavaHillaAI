4f2b99649f13ab1b5738d9b68305758544ffce5b
Jason Rathgeber <jrathgeber@yahoo.com>
2024-06-24 22:29:24 -0400

    combo box

--- frontend/views/home/HomeView.tsx
+++ frontend/views/home/HomeView.tsx
@@ -1,8 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { ComboBox } from '@hilla/react-components/ComboBox.js';
+import { HomeService } from 'Frontend/generated/endpoints.js';
+
 export default function HomeView() {
+
+  const [items, setItems] = useState<String[]>([]);
+
+  useEffect(() => {
+    HomeService.getCountries().then((countries) => setItems(countries));
+  }, []);
+
+
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
       <h2>Welcome to Chat with CLO</h2>
       <p>🤗 Jason Rathgeber 🤗</p>
+
+      <ComboBox
+        allowCustomValue
+        label="Model"
+        helperText="Select or type a model"
+        items={items}
+      ></ComboBox>
+
     </div>
   );
 }
---
+++ src/main/java/com/example/application/services/HomeService.java
@@ -0,0 +1,35 @@
+package com.example.application.services;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import dev.hilla.BrowserCallable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@BrowserCallable
+@AnonymousAllowed
+@Service
+public class HomeService {
+
+    private List<String> stringList;
+
+    HomeService() {
+
+        stringList = new ArrayList<>();
+
+        stringList.add("Gpt 3.5 turbo");
+        stringList.add("Gpt 4o");
+        stringList.add("Claude 3.5");
+
+    }
+
+    public List<String> getCountries() {
+
+        return stringList;
+
+    }
+
+
+}
